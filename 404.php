e9c5988b25e9826daac0558261462bdfefe1f269
e1754709 <e1754709@cmaisonneuve.qc.ca>
2023-04-18 15:16:25 -0400

    ajout couleurs et flex sur les menus

--- 404.php
+++ 404.php
@@ -3,19 +3,20 @@
 ?>
 <?php get_header(); ?>
 <main class="site__main">
-    <h1>Page introuvable, vous pouvez tenter une recherche</h1>
-    <?= get_search_form(); ?>
-    <h2>Nos choix de cours</h2>
-
-    <?php wp_nav_menu(array(
-        "menu" => "cours",
-        "container" => "nav"
-    )); ?>
-    <h2>Les notes de cours</h2>
-    <?php wp_nav_menu(array(
-        "menu" => "notes-wp",
-        "container" => "nav"
-    )); ?>
-        
+    <div class="page404">
+        <h1>Erreur 404</h1>
+        <h2>Page introuvable, vous pouvez tenter une recherche</h2>
+        <?= get_search_form(); ?>
+        <h2>Nos choix de cours</h2>
+        <?php wp_nav_menu(array(
+            "menu" => "cours",
+            "container" => "nav"
+        )); ?>
+        <h2>Les notes de cours</h2>
+        <?php wp_nav_menu(array(
+            "menu" => "note-wp",
+            "container" => "nav"
+        )); ?>
+    </div>   
 </main>
 <?php get_footer(); ?>
