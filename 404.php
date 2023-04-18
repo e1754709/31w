2aa718a714e516fe6bbf6d86de2dcfb6b78a86bc
e1754709 <e1754709@cmaisonneuve.qc.ca>
2023-04-18 14:17:33 -0400

    ajout des menus et du search bar

--- 404.php
+++ 404.php
@@ -2,9 +2,20 @@
     //Ceci est notre modele.
 ?>
 <?php get_header(); ?>
-<main class="site__main">
-    <code>404.php</code>
-    <h1>Erreur 404 - La page n'existe pas</h1>
+<main class="site__main no-aside">
+    <h1>Page introuvable, vous pouvez tenter une recherche</h1>
+    <?= get_search_form(); ?>
+    <h2>Nos choix de cours</h2>
+
+    <?php wp_nav_menu(array(
+        "menu" => "cours",
+        "container" => "nav"
+    )); ?>
+    <h2>Les notes de cours</h2>
+    <?php wp_nav_menu(array(
+        "menu" => "notes-wp",
+        "container" => "nav"
+    )); ?>
         
 </main>
 <?php get_footer(); ?>
